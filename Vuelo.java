dd7fac4a7993a111da7f79164318105e66b31ebd
mariajperezgo-oss <mariaj.perezgo@autonoma.edu.co>
2026-03-16 16:17:17 -0500

    Update Vuelo.java

--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,28 @@
-public class Vuelo{
 
-  
+
+  public class Vuelo{
+
+    protected String origen;
+    protected String destino;
+    protected String fecha;
+    protected double precio;
+
+    public Vuelo(String origen, String destino, String fecha, double precio){
+        this.origen = origen;
+        this.destino = destino;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+
+    public String getOrigen(){return origen;}
+    public String getDestino(){return destino;}
+    public String getFecha(){return fecha;}
+    public double getPrecio(){return precio;}
+
+    public void mostrarInfo(){
+        System.out.println("Vuelo: " + origen + " -> " + destino);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Precio: " + precio);
+    }
 }
+
