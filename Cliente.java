525c64775b18988bbb4010de89f478a428d2c021
el pibe <santiago.castanoca@autonoma.edu.co>
2026-03-17 19:42:43 -0500

    Agregados nuevos metodos a Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -7,7 +7,6 @@ public class Cliente {
     protected String telefono;
     protected String cedula;
     
-    
     public Cliente(String nombre, String apellido, String telefono,String cedula ) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -28,4 +27,19 @@ public class Cliente {
     public void mostrarInfo() {
         System.out.println("Nombre: " + getNombreCompleto());
         System.out.println("Telefono: " + telefono);
+    
     }
+    
+    
+    public void saludar() {
+        System.out.println("Hola, soy " + nombre + " y soy cliente de la agencia");
+    }
+    
+    public void mostrarCedula() {
+        System.out.println("Mi cédula es: " + cedula);
+    }
+    
+    public void despedirse() {
+        System.out.println("Gracias por su atención, que tenga un buen día");
+    }
+}
