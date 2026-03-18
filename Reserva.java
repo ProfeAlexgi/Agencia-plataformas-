ca37aac1100c3af060b3ff62b2425c9d2d9ad0fc
luis angel <refinadiaz@gmail.com>
2026-03-17 19:43:32 -0500

    hola, modifique reserva y le agregue el metodo crearReserva

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,16 @@
 public class Reserva{
 
+    public String crearReserva() {
+        if (hotel.getHabitacionesDisponibles() >= cantidadHabitaciones) {
+            hotel.setHabitacionesDisponibles(
+                hotel.getHabitacionesDisponibles() - cantidadHabitaciones
+            );
+            confirmada = true;
+            return "Reserva confirmada para " + cliente.getNombre();
+        } else {
+            return "No hay habitaciones disponibles";
+        }
+    }
 
   
 }
