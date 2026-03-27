dbf5e3462539e0f0e3995e9d238a169c5dffe3a3
Veronica Mejia <veronica.mejiam@autonoma.edu.co>
2026-03-26 20:28:33 -0500

    mejoras en clase Cliente

--- Cliente.java
+++ Cliente.java
@@ -3,10 +3,14 @@ public class Cliente {
     protected String nombre;
     protected String apellido;
     protected String telefono;
-    protected String cedula;
+    protected int cedula;
+    protected String email;
+    protected String contrasenia;
+    protected String usuario;
+
     
     
-    public Cliente(String nombre, String apellido, String telefono,String cedula ) {
+    public Cliente(String nombre, String apellido, String telefono,int cedula ) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
@@ -16,22 +20,31 @@ public class Cliente {
     public String getNombre(){return nombre;}
     public String getApellido(){return apellido;}
     public String getTelefono(){return telefono;}
-    public String getCedula(){return cedula;}
+    public int getCedula(){return cedula;}
      
      
     public String getNombreCompleto() {
         return nombre + " " + apellido;
     }
+    public void actualizarDatos(String nuevoUsuario, String nuevacontrasenia, String nuevoTelefono, String nuevoEmail) {
+       this.usuario= nuevoUsuario;
+       this.contrasenia = nuevacontrasenia;
+       this.telefono = nuevoTelefono;
+       this.email = nuevoEmail;
+
+    }
+
 
     public void mostrarInfo() {
         System.out.println("Nombre: " + getNombreCompleto());
         System.out.println("Telefono: " + telefono);
     }
-    public RegistraCliente(){
+    public void  RegistraCliente(){
 
         System.out.println("El cliente ha sido registrado exitosamente");    
     }
-    public void MostrarInformacion() {System.out.println(Informacion del clliente \n};
-     {System.out.println(nombre+""+apellido""+correo)};
-
- 
+    public void MostrarInformacion() {
+        System.out.println("Informacion del cliente \n");
+        System.out.println(nombre + " " + apellido + " " + email);
+    }
+    }
